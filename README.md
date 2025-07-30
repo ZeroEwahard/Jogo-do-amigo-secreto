0af566d294df25cf4e3a6d30c2ac6db0ee2f78c7
ZeroEwahard <ryan536pereira@gamil.com>
2025-07-30 14:23:51 -0300

    Finaliza funcionalidades

---
+++ README.md
@@ -0,0 +1,35 @@
+# Amigo Secreto 🎁
+
+
+Projeto em JavaScript, HTML e CSS para criar uma 
+lista de amigos e sortear aleatoriamente um nome da lista. 
+
+
+## 💡 Funcionalidades 
+
+- Adicionar nomes de amigos
+- Sortear um amigo aleatoriamente 
+- Exibir a lista com os nomes dos amigos
+- Interface Amigável
+
+
+## 🛠️ Tecnologias usadas
+
+- HTML
+- CSS
+- JavaScript
+
+
+## ➡️ Como usar
+
+1- Digite o nome de um amigo no campo de entrada 
+2- Clique em **"Adicionar"**
+3- Após adicionar todos, clique em **"Sortear"**
+4- O nome sorteador aparecerá abaixo 
+
+
+## 📁 Arquivos principais 
+
+- `index.html`: estrutura da página
+- `style.css`: estilos visuais 
+- `app.js`: lógica em JavaScript
---
+++ app.js
@@ -0,0 +1,75 @@
+let amigos = [];
+
+
+function adicionarAmigo() {
+
+    let digiteNomeDoAmigo = document.getElementById("amigo").value;
+
+    if(digiteNomeDoAmigo != "") {
+        amigos.push(digiteNomeDoAmigo);
+        limparInput();
+        atualizarListaDeAmigo();
+
+    } else {
+        alert(`Por favor, insira um nome.`);
+    }
+}
+
+
+function limparInput() {
+    let limpar = document.querySelector("input");
+    limpar.value = "";
+}
+
+
+function atualizarListaDeAmigo() {
+
+    let amigoAdicionado = document.getElementById("listaAmigos");
+    let lista; 
+    amigoAdicionado.innerHTML = "";
+
+    for(let quantidadeDeAmigos = 0; quantidadeDeAmigos < amigos.length; quantidadeDeAmigos++) {
+        lista = document.createElement("li");
+        lista.textContent = amigos[quantidadeDeAmigos];
+        amigoAdicionado.appendChild(lista);
+        estilizarLista();
+    }
+
+
+    function estilizarLista() {
+        lista.style.background = "#4b69fd";
+        lista.style.color = "white";
+        lista.style.fontStyle = "italic";
+        lista.style.padding = "5px";
+        lista.style.marginBottom = "5px";
+        lista.style.borderRadius = "6px";
+        lista.style.border = "2px solid #2a3dbf";
+        lista.style.textAlign = "center";
+    } 
+}
+
+
+function sortearAmigo() {
+
+    if(amigos.length > 0) {
+       let sorteio = Math.floor(Math.random() * amigos.length);
+       let resultado = document.getElementById("resultado");
+       resultado.innerHTML = amigos[sorteio];
+       
+
+       function estilizarResultado() {
+        resultado.style.background = "#a8e6cf";
+        resultado.style.fontWeight = "bold";
+        resultado.style.border = "2px solid #81c784";
+        resultado.style.borderRadius = "8px";
+        resultado.style.padding = "6px 10px";
+        resultado.style.marginBottom = "6px";
+        resultado.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.2)";
+        }
+    
+        estilizarResultado();
+
+    } else {
+        alert(`Você precisa colocar o nome de um amigo no campo de texto para poder prosseguir.`);
+    }
+}
